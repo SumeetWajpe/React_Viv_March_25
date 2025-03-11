d9a7d7ec1b5f278f8270a5e3acca17b1e4d08777
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-03-11 10:06:10 +0530

    ✨ using Create React App for creating first react app

---
+++ DAY2/CRA/my--first-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <h1> Hello Create React App !</h1>;
+  }
+}
+
+export default App;
