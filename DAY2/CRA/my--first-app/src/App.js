b597774840712f58a474128592d6a6a94144de13
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-03-11 10:57:40 +0530

    ✨ using props to set src of image

--- DAY2/CRA/my--first-app/src/App.js
+++ DAY2/CRA/my--first-app/src/App.js
@@ -6,10 +6,20 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Message msg="Hello" to="John" from="Carter" />
-        <Message msg="Hey" to="Peter" from="Parker" />
-        <Message msg="Hi" to="James" from="Bond" />
-        <Message msg="Hola" to="John" from="McLaren" />
+        <Message
+          msg="Hello"
+          to="John"
+          from="Carter"
+          imageUrl="https://thumb.ac-illust.com/e2/e2cbae08aee6ed3c5fa742b33e936831_t.jpeg"
+        />
+        <Message
+          msg="Hey"
+          to="Peter"
+          from="Parker"
+          imageUrl="https://t4.ftcdn.net/jpg/02/19/68/97/360_F_219689742_N53gBb5bVYPzWTOaps8A2LAUx62jbvWo.jpg"
+        />
+        {/* <Message msg="Hi" to="James" from="Bond" />
+        <Message msg="Hola" to="John" from="McLaren" /> */}
       </div>
     );
   }
--- DAY2/CRA/my--first-app/src/Message.jsx
+++ DAY2/CRA/my--first-app/src/Message.jsx
@@ -4,6 +4,7 @@ class Message extends React.Component {
   render() {
     return (
       <div>
+        <img src={this.props.imageUrl} alt="" width="150px" height="100px" />
         <h1>{this.props.msg}</h1>
         <p>From : {this.props.from}</p>
         <p>To : {this.props.to}</p>
