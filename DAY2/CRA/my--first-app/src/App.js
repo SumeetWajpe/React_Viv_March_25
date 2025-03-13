c740b2f7c7891e9319ac3a8c4238adbb12e98b55
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-03-13 10:24:58 +0530

    ✨ Using bootstrap card for displaying messages

--- DAY2/CRA/my--first-app/src/App.js
+++ DAY2/CRA/my--first-app/src/App.js
@@ -19,10 +19,38 @@ class App extends React.Component {
       imageUrl:
         "https://t4.ftcdn.net/jpg/02/19/68/97/360_F_219689742_N53gBb5bVYPzWTOaps8A2LAUx62jbvWo.jpg",
     },
+    {
+      msg: "Hello",
+      to: "John",
+      from: "Carter",
+      imageUrl:
+        "https://thumb.ac-illust.com/e2/e2cbae08aee6ed3c5fa742b33e936831_t.jpeg",
+    },
+    {
+      msg: "Hey",
+      to: "Peter",
+      from: "Parker",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/02/19/68/97/360_F_219689742_N53gBb5bVYPzWTOaps8A2LAUx62jbvWo.jpg",
+    },
+    {
+      msg: "Hello",
+      to: "John",
+      from: "Carter",
+      imageUrl:
+        "https://thumb.ac-illust.com/e2/e2cbae08aee6ed3c5fa742b33e936831_t.jpeg",
+    },
+    {
+      msg: "Hey",
+      to: "Peter",
+      from: "Parker",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/02/19/68/97/360_F_219689742_N53gBb5bVYPzWTOaps8A2LAUx62jbvWo.jpg",
+    },
   ];
   render() {
     return (
-      <div>
+      <div className="row">
         {this.messages.map(m => (
           <Message messagedetails={m} />
         ))}
--- DAY2/CRA/my--first-app/src/Message.jsx
+++ DAY2/CRA/my--first-app/src/Message.jsx
@@ -3,30 +3,19 @@ import "./message.css";
 class Message extends React.Component {
   render() {
     return (
-      // <div
-      //   style={{ border: "1px solid grey", margin: "10px", padding: "10px" }}
-      // >
-      <div className="message-item">
-        {/* {this.props.imageUrl ? (
-          <img src={this.props.imageUrl} alt="" width="150px" height="100px" />
-        ) : (
+      <div className="col-md-3">
+        <div className="card" style={{ width: "18rem", height: "20rem" }}>
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
-            alt=""
-            width="150px"
-            height="100px"
+            src={this.props.messagedetails.imageUrl}
+            className="card-img-top"
+            alt="..."
           />
-        )} */}
-        <img
-          src={this.props.messagedetails.imageUrl}
-          alt=""
-          width="150px"
-          height="100px"
-        />
-
-        <h1>{this.props.messagedetails.msg}</h1>
-        <p>From : {this.props.messagedetails.from}</p>
-        <p>To : {this.props.messagedetails.to}</p>
+          <div className="card-body">
+            <h5 className="card-title">{this.props.messagedetails.msg}</h5>
+            <p className="card-text">To: {this.props.messagedetails.to}</p>
+            <p className="card-text">From: {this.props.messagedetails.from}</p>
+          </div>
+        </div>
       </div>
     );
   }
