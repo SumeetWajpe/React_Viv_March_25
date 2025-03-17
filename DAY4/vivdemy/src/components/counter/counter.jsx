fa9694027104019b887616ea81c354960781c96c
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-03-17 11:43:22 +0530

    ✨ using multiple useState

--- DAY4/vivdemy/src/components/counter/counter.jsx
+++ DAY4/vivdemy/src/components/counter/counter.jsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 
 var Counter = () => {
   var [count, setCount] = useState({ currCount: 10 }); // useState is available only from 16.8 version of react - Hook
+  var [age, setAge] = useState(18);
   return (
     <>
       <p>Count : {count.currCount}</p>
       <button
         className="btn btn-primary"
         onClick={() => {
-          setCount({ currCount: count.currCount + 1 });
+          setCount({ currCount: count.currCount + 10 });
         }}
       >
         Increment++
+      </button>
+      <hr />
+      <p>Age : {age}</p>
+      <button className="btn btn-primary" onClick={() => setAge(age + 1)}>
+        Age++
       </button>
     </>
   );
