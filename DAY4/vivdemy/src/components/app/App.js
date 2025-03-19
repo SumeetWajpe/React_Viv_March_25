cdbaa7ec0c4ca9d51b0cba6e297a9ea19397309b
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-03-19 11:08:37 +0530

    ✨ Routing Basics

--- DAY4/vivdemy/src/components/app/App.js
+++ DAY4/vivdemy/src/components/app/App.js
@@ -3,10 +3,29 @@ import ListOfCourses from "../listofcourses/listofcourses";
 import Message from "../message/message";
 import Counter from "../counter/counter";
 import Posts, { Add } from "../posts/posts";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <ListOfCourses />;
-  // return <Posts />;
+  return (
+    <>
+      <BrowserRouter>
+        {/* <a href="/">Courses</a> | <a href="/posts">Posts</a> */}
+        <Link to="/">Courses</Link> | <Link to="/posts">Posts</Link>
+        <Routes>
+          <Route path="/" Component={ListOfCourses} />
+          <Route path="/posts" Component={Posts} />
+          <Route path="/counter" Component={Counter} />
+
+          <Route
+            path="*"
+            element={
+              <img src="https://internetdevels.com/sites/default/files/public/blog_preview/404_page_cover.jpg" />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
