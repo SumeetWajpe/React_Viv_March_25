aa0236a8f6fef608d7c62f5ba2fb3ebc1abc96e7
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-03-21 11:48:39 +0530

    ✨ Adding New Course

--- DAY4/vivdemy/src/components/newcourse/newcourse.jsx
+++ DAY4/vivdemy/src/components/newcourse/newcourse.jsx
@@ -1,8 +1,9 @@
+import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function NewCourse() {
-  // const [id, setId] = useState("");
-  // const [title, setTitle] = useState("");
+  const navigate = useNavigate();
 
   const [newcourse, setnewCourse] = useState({});
 
@@ -13,6 +14,12 @@ function NewCourse() {
         onSubmit={e => {
           e.preventDefault(); // PREVENT THE PAGE FROM REFRESHING
           console.log(newcourse);
+
+          axios.post("http://localhost:3500/courses", newcourse).then(res => {
+            if (res.data.title == newcourse.title) {
+              navigate("/"); // programatic navigation
+            }
+          });
         }}
       >
         <label htmlFor="txtCourseId"> Id : </label>
@@ -29,7 +36,47 @@ function NewCourse() {
           onInput={e => setnewCourse({ ...newcourse, title: e.target.value })}
         />
         <br />
-        <button className="btn btn-success">Add New Course</button>
+        <label htmlFor="txtCoursePrice"> Price : </label>
+        <input
+          type="number"
+          id="txtCoursePrice"
+          onInput={e => setnewCourse({ ...newcourse, price: +e.target.value })}
+        />
+        <br />
+        <label htmlFor="txtCourseRating"> Rating : </label>
+        <input
+          type="number"
+          id="txtCourseRating"
+          onInput={e => setnewCourse({ ...newcourse, rating: +e.target.value })}
+        />
+        <br />
+        <label htmlFor="txtCourseLikes"> Likes : </label>
+        <input
+          type="number"
+          id="txtCourseLikes"
+          onInput={e => setnewCourse({ ...newcourse, likes: +e.target.value })}
+        />
+        <br />
+        <label htmlFor="txtCourseImageUrl"> Image Url : </label>
+        <input
+          type="text"
+          id="txtCourseImageUrl"
+          onInput={e =>
+            setnewCourse({ ...newcourse, imageUrl: e.target.value })
+          }
+        />
+        <br />
+        <label htmlFor="txtCourseDescription"> Description : </label>
+        <textarea
+          rows={5}
+          id="txtCourseDescription"
+          onInput={e =>
+            setnewCourse({ ...newcourse, description: e.target.value })
+          }
+        ></textarea>
+        <br />
+
+        <button className="btn btn-success mt-2">Add New Course</button>
       </form>
     </div>
   );
